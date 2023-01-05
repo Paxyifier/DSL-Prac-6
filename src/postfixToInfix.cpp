63cfc96e6c2f549893e29364c15314101affa345
Paxyifier <59118083+Paxyifier@users.noreply.github.com>
2023-01-05 17:39:07 +0000

    postfix done

--- src/postfixToInfix.cpp
+++ src/postfixToInfix.cpp
@@ -4,24 +4,20 @@ using namespace std;
 
 class Node{
     protected:
-        int data;
+        char data;
         Node *next;
     public:
-        Node(int data){
+        Node(char data){
             this->data = data;
         }
-        Node(int data, Node *next){
+        Node(char data, Node *next){
             this->data = data;
             this->next = next;
         }
-        Node(){
-            this->data = 0;
-            this->next = NULL;
-        }
-        void setData(int data){
+        void setData(char data){
             this->data = data;
         }
-        int getData(){
+        char getData(){
             return this->data;
         }
         void setNext(Node *next){
@@ -40,21 +36,30 @@ class Stack{
                 this->top = NULL;
                 this->size = 0;
             }
-            void push(int data){
+            void push(char data){
                 Node *new_node = new Node(data);
                 new_node->setNext(top);
                 top = new_node;
                 this->size++;
             }
-            void pop(){
+            char pop(){
                 if(top == NULL){
                     cout << "Stack Underflow" << endl;
-                    return;
+                    return '`';
                 }
                 Node *temp = top;
+                char chr = this->top->getData();
                 top = top->getNext();
                 this->size--;
                 delete temp;
+                return chr;
+            }
+            char head() {
+                if(top == NULL){
+                    // cout << "Stack Underflow" << endl;
+                    return NULL;
+                }
+                return top->getData();
             }
             void display(){
                 if(top == NULL){
@@ -86,14 +91,87 @@ class Stack{
 };
 class Postfix{
     protected:
+        string output;
         Stack  *stack;
     public:
-        Postfix(){
+        Postfix(string infix){
             this->stack = new Stack();
+            for(int i=0; i<infix.size(); i++){
+                switch(infix[i]){
+                    case '(':
+                        this->stack->push('(');
+                        break;
+                    case ')':
+                        while(this->stack->head() != '(' && this->stack->head() != NULL){
+                            char c = this->stack->pop();
+                            output.append(1,c);
+                        }
+                        this->stack->pop();
+                        break;
+                    case '+':
+                        popTillPrecedence(infix[i]);
+                        this->stack->push('+');
+                        break;
+                    case '-':
+                        popTillPrecedence(infix[i]);
+                        this->stack->push('-');
+                        break;
+                    case '*':
+                        popTillPrecedence(infix[i]);
+                        this->stack->push('*');
+                        break;
+                    case '/':
+                        popTillPrecedence(infix[i]);
+                        this->stack->push('/');
+                        break;
+                    case '^':
+                        popTillPrecedence(infix[i]);
+                        this->stack->push('^');
+                        break;
+                    case '%':
+                        this->stack->push('%');
+                        break;
+                    default:
+                        this->output.append(1,infix[i]);
+                        break;
+                }
+            }
+            if (this->stack->getSize() != 0) {
+                cout << "Equation is not balanced.\n";
+                exit(0);
+            }
         }
-        
+        int precedence(char op){
+            switch(op){
+                case '+':
+                    return 0;
+                case '-':
+                    return 1;
+                case '*':
+                    return 2;
+                case '/':
+                    return 3;
+                case '^':
+                    return 4;
+                case '%':
+                    return 5;
+                default:
+                    return -1;
+            }
+        }
+        void popTillPrecedence(char op){
+            while(precedence(this->stack->head())>precedence(op) && this->stack->head() != NULL){
+                this->output.append(1,this->stack->pop());
+            }
+        }
+        void display(){
+            cout<<output<<endl;
+        }
 };
 
 int main(){
-
+    string str = "(A/(B-C)*D+E)";
+    Postfix pf = Postfix(str);
+    pf.display();
+    return 0;
 }
